ab0ab535539a466495bba316eb5c60ca905a56d5
Kartib B <work.kartikb@gmail.com>
2023-07-19 15:59:08 +0530

    new procedures added

--- database/db_changes.sql
+++ database/db_changes.sql
@@ -1,1 +1,14 @@
 ALTER TABLE `messages` ADD `grp_member_id` INT(11) NULL DEFAULT '0' AFTER `body`;
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `get_group_messages`(IN `group_idx` INT)
+BEGIN
+SELECT * FROM `messages` WHERE `type`='group' AND `sent_to_id`=`group_idx`;
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `get_user_info_by_id`(IN `idx` INT(11))
+BEGIN
+SELECT * FROM `users` WHERE `id`=`idx`;
+END$$
+DELIMITER ;
