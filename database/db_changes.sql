b03381f5b276ffccd813287112a8d02989ec58be
Kartik Bharne <kartiktheno1king@gmail.com>
2023-07-21 00:03:01 +0530

    modified procedure

--- database/db_changes.sql
+++ database/db_changes.sql
@@ -12,3 +12,10 @@ BEGIN
 SELECT * FROM `users` WHERE `id`=`idx`;
 END$$
 DELIMITER ;
+
+
+DROP PROCEDURE `list_user`; 
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `list_user`() NOT DETERMINISTIC CONTAINS SQL SQL SECURITY DEFINER BEGIN SELECT id,fname,lname,status,COALESCE(image,'') as image FROM `users` WHERE `is_deleted` = 0 AND `status` = 1 ORDER BY `users`.`id` DESC;
+END$$
+DELIMITER ;
